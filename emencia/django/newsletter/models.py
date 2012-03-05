537e1bb5697ba7b7f66136cbde1d686dec83fd49
daniel <daniel@freshmilk.tv>
2012-03-05 16:24:37 +0100

    Change first/last name order in contact.mail_format

--- emencia/django/newsletter/models.py
+++ emencia/django/newsletter/models.py
@@ -141,7 +141,7 @@ class Contact(models.Model):
 
     def mail_format(self):
         if self.first_name and self.last_name:
-            return '%s %s <%s>' % (self.last_name, self.first_name, self.email)
+            return '%s %s <%s>' % (self.first_name, self.last_name, self.email)
         return self.email
     mail_format.short_description = _('mail format')
 
